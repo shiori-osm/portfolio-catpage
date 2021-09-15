0fdf87f393e7ed66e31498d92543d79f84788958
しおり.大隅 <metalgear.shiorid@gmail.com>
2021-09-15 22:44:35 +0900

    jQueryとdisplay none

---
+++ js/my-jquery.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+  $('header').hide().fadeIn(2500);
+});
+
+jQuery(function ($) {
+  var fadeIn = $('.fade-in');
+  $(window).scroll(function () {
+    $(fadeIn).each(function () {
+      var offset = $(this).offset().top;
+      var scroll = $(window).scrollTop(); 
+      var windowHeight = $(window).height();
+      if (scroll > offset - windowHeight + 150) {
+        $(this).addClass("scroll-in");
+      }
+    });
+  });
+});
