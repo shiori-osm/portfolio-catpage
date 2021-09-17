b7474ffb52646de48aaf8f74d70ab4032ef42921
しおり.大隅 <metalgear.shiorid@gmail.com>
2021-09-18 08:57:14 +0900

    新たな問題header-spが表示されない

--- js/my-jquery.js
+++ js/my-jquery.js
@@ -1,18 +1,21 @@
 
 $(document).ready(function() {
   $('header').hide().fadeIn(2500);
+  $('header-sp').hide().fadeIn(2500);
 });
 
+
 jQuery(function ($) {
-  var fadeIn = $('.fade-in');
+  const fadeIn = $('.fade-in');
   $(window).scroll(function () {
     $(fadeIn).each(function () {
-      var offset = $(this).offset().top;
-      var scroll = $(window).scrollTop(); 
-      var windowHeight = $(window).height();
+      const offset = $(this).offset().top;
+      const scroll = $(window).scrollTop(); 
+      const windowHeight = $(window).height();
       if (scroll > offset - windowHeight + 150) {
         $(this).addClass("scroll-in");
       }
     });
   });
 });
+
